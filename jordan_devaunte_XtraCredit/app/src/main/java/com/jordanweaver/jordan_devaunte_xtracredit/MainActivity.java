8e614ab2df75d78e7baf9013e32ebfc97a99ab6a
dledee1995 <devaunteledee@DeVauntes-MacBook-Pro.local>
2015-02-23 22:33:08 -0500

    Devaunte Progress Push

--- jordan_devaunte_XtraCredit/app/src/main/java/com/jordanweaver/jordan_devaunte_xtracredit/MainActivity.java
+++ jordan_devaunte_XtraCredit/app/src/main/java/com/jordanweaver/jordan_devaunte_xtracredit/MainActivity.java
@@ -1,7 +1,6 @@
 package com.jordanweaver.jordan_devaunte_xtracredit;
 
 import android.app.Activity;
-<<<<<<< HEAD
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -9,9 +8,17 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,35 +27,41 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-=======
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
->>>>>>> developmentBranch
+import java.util.ArrayList;
+
+//<<<<<<< HEAD
 
 
 public class MainActivity extends Activity {
 
 
     Spinner weatherSpinner;
+    //Context mContext;
     Context mContext;
-
+    LinearLayout currentWeatherView;
+    LinearLayout hourlyWeatherView;
+    LinearLayout tenDayWeatherView;
+    TextView locationText;
+    TextView currentCondition;
+    TextView degrees;
+    ListView hourlyListView;
+    ArrayList<weatherClass> myWeatherClass = new ArrayList<weatherClass>();
+    // 2nd Page
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
+
 
         weatherSpinner = (Spinner) findViewById(R.id.weatherSpinner);
-
+        currentWeatherView = (LinearLayout) findViewById(R.id.currentWeatherView);
+        hourlyWeatherView = (LinearLayout) findViewById(R.id.hourlyWeatherView);
+        tenDayWeatherView = (LinearLayout) findViewById(R.id.tenDayWeatherView);
+        locationText = (TextView) findViewById(R.id.cityLabel);
+        currentCondition = (TextView) findViewById(R.id.conditionLabel);
+        degrees = (TextView) findViewById(R.id.degreesLabel);
+        hourlyListView = (ListView) findViewById(R.id.hourlyList);
         mContext = this;
 
 
@@ -59,7 +72,41 @@ public class MainActivity extends Activity {
         );
 
         weatherSpinner.setAdapter(spinnerAdapter);
->>>>>>> developmentBranch
+
+         weatherSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+             @Override
+             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                 switch (position){
+                     case 0:
+                         currentWeatherView.setVisibility(View.VISIBLE);
+                         hourlyWeatherView.setVisibility(View.GONE);
+                         tenDayWeatherView.setVisibility(View.GONE);
+                         break;
+                     case 1:
+
+                         hourlyWeatherView.setVisibility(View.VISIBLE);
+                         currentWeatherView.setVisibility(View.GONE);
+                         tenDayWeatherView.setVisibility(View.GONE);
+                         hourlyAdapter myAdapter = new hourlyAdapter(mContext, myWeatherClass);
+                         hourlyListView.setAdapter(myAdapter);
+
+                         break;
+                     case 2:
+                         tenDayWeatherView.setVisibility(View.VISIBLE);
+                         currentWeatherView.setVisibility(View.GONE);
+                         hourlyWeatherView.setVisibility(View.GONE);
+                 }
+
+
+             }
+
+             @Override
+             public void onNothingSelected(AdapterView<?> parent) {
+
+             }
+         });
+
+
 
         ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (connectivityManager != null) {
@@ -67,14 +114,15 @@ public class MainActivity extends Activity {
             if (info != null && info.isConnected()) {
                 MyTask myTask = new MyTask();
 
-<<<<<<< HEAD
-                myTask.execute("http://api.wunderground.com/api/4bfff3e6246abd8a/conditions/q/FL/orlando.json");
+                myTask.execute("http://api.wunderground.com/api/4bfff3e6246abd8a/conditions/forecast10day/hourly/settings/q/FL/orlando.json");
 
             } else {
-                Toast.makeText(MainActivity.this, "Internet connection unavalable", Toast.LENGTH_LONG);
+                Toast.makeText(MainActivity.this, "Internet connection unavailable", Toast.LENGTH_LONG).show();
             }
         }
     }
+
+
 
     public class MyTask extends AsyncTask<String, Void, String> {
 
@@ -138,7 +186,6 @@ public class MainActivity extends Activity {
 
 
                     Log.e("Json array", "Children array");
-//                    JSONArray BookArray = mainObject.getJSONArray("items");
                     JSONObject theweatherObject = mainObject.getJSONObject("current_observation");
                     JSONObject weatherObject = mainObject.getJSONObject("current_observation").getJSONObject("display_location");
 
@@ -151,53 +198,101 @@ public class MainActivity extends Activity {
 
                         String condition;
                         String temperature;
-                        String myImageLink = "";
-//                        JSONObject childObjectPicture = BookArray.getJSONObject(i).getJSONObject("volumeInfo").getJSONObject("imageLinks");
 
 
 
-                    //Second screen is an hourly forecast shown in a list.
 
 
 
 
                     //
                         // TODO Log
-                        Log.e("IF check for supreddit", "Subreddit");
                         if (weatherObject.has("full")) {
                             location = weatherObject.getString("full");
-
+                            locationText.setText(location);
+                            Log.e("City", location+"");
 
                         } else {
                             location = "N/A";
+                            Log.e("City", location+"");
                         }
-                        if (theweatherObject.has("temperature_string")) {
-                            temperature = theweatherObject.getJSONArray("temperature_string").toString();
-                            Log.i("Authors name", "............ AuthorsName:" + temperature);
-//                            authorsName = childObject.getString("authors");
+                    if (theweatherObject.has("feelslike_f")) {
+                        temperature = theweatherObject.getString("feelslike_f") + " F";
+                        degrees.setText(temperature);
+
+
+                            Log.e("City", temperature+"");
 
                         } else {
                             temperature = "N/A";
+                            Log.e("City", temperature+"");
                         }
 
-                        if (theweatherObject.has("description")) {
+                        if (theweatherObject.has("weather")) {
                             condition = theweatherObject.getString("weather");
+                            currentCondition.setText(condition);
                         } else {
                             condition = "N/A";
+                            Log.e("City", condition+"");
+
+                        }
+                    //Second screen is an hourly forecast shown in a list.
+
+
+                    JSONArray theHourlyweatherArray = mainObject.getJSONArray("hourly_forecast");
+
+
+                    // TODO Log
+                    Log.e("Going to for loop", "For loop");
+                    for (int i = 0; i < theHourlyweatherArray.length(); i++) {
+                        JSONObject temp = theHourlyweatherArray.getJSONObject(i).getJSONObject("temp");
+
+
+                        String hour = null;
+                        String high;
+                        String Low;
+                        String hourlyCondition;
+
+                        JSONObject Objects = theHourlyweatherArray.getJSONObject(i);
+                        // TODO Log
+                         Log.e("I AM IN THE FOR LOOP");
+
+                        if (temp.has("english")) {
+
+                            high = temp.getString("english");
+
+
+                        } else {
+                            high = "N/A";
+                            Log.i("high ", "............ high:" + high);
+                        }
+                        if (temp.has("dewpoint")) {
+                            Low = temp.getJSONArray("english").toString();
+                            Log.i("Low", "............ Low:" + Low);
+//                            authorsName = childObject.getString("authors");
+
+                        } else {
+                            Low = "N/A";
+                            Log.i("Low:", "............ Low:" + Low);
+                        }
+
+                        if (Objects.has("condition")) {
+                            hourlyCondition = Objects.getString("condition");
+                        } else {
+                            hourlyCondition = "N/A";
+                            Log.i("hourly Conditon", "............ hourly Conditon:" + hourlyCondition);
 
                         }
 
-//                        if (childObject.has("imageLinks")) {
-//
-//                            //image.setImageUrl(childObjectPicture.getString("smallThumbnail"));
-//                            myImageLink = childObjectPicture.getString("thumbnail");
-//                            Log.i("Image being passed", "Image being passed:" + myImageLink);
-//                        }
+                        if (Objects.getJSONObject("FCTTIME").has("civil")) {
+                            hour = Objects.getString("civil");
 
-//                        books.add(new Book(bookName, description, myImageLink, authorsName));
+                        }
+
+                        myWeatherClass.add(new weatherClass(high,Low,hour,hourlyCondition));
 
 
-
+                    }
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -205,9 +300,6 @@ public class MainActivity extends Activity {
 
 
 
-//                MyAdapter adapter = new MyAdapter(MainActivity.this, books);
-//
-//                gridView.setAdapter(adapter);
 
                 dialog.cancel();
             }
@@ -216,9 +308,7 @@ public class MainActivity extends Activity {
         }
 
 
-=======
 
->>>>>>> developmentBranch
     }
 
 
